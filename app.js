08ff31c6eab69ec3ece7f975fbd9748104a0031c
Az <aztecian1122@gmail.com>
2019-06-28 12:53:12 +0100

    adding gridcells styles to display

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-const btnReset = document.getElementById('reset');
-const btnBlack = document.getElementById('black');
-const btnRainbow = document.getElementById('rainbow');
-const container = document.querySelector('#container');
-let grid = 16;
+//global scope
+const btnReset = document.getElementById("reset");
+const btnBlack = document.getElementById("black");
+const btnRainbow = document.getElementById("rainbow");
+const container = document.querySelector("#container");
+
+//container styles.
+document.getElementById("container").style.display = "flex";
+document.getElementById("container").style.flexFlow = "row wrap";
+document.getElementById("container").style.justifyContent = "flex-start";
+document.getElementById("container").style.border = "1px solid #000";
+document.getElementById("container").style.height = "600px";
+document.getElementById("container").style.width = "600px";
+document.getElementById("container").style.margin = "auto";
 
 //create grid
-function createGrid(grid) {
-    resetContainer();
+function createGrid(gSize) {
+  for (let i = 0; i < gSize; i++) {
+    let gridCells = document.createElement("div");
+    gridCells.className = "grid-cell";
+    gridCells.style.backgroundColor = "white";
+    gridCells.style.width = "50px";
+    gridCells.style.height = "50px";
+    gridCells.style.margin = "0";
+    gridCells.style.padding = "0";
+    gridCells.style.border = " 1px solid #000";
 
-    for (let i = 0; i < (grid * grid); i++) {
-        const cell = document.createElement('div');
-        cell.className = 'grid-cell';
-        cell.style.color = 'red';
-
-
-        container.appendChild(cell);
-    }
-    container.style.gridTemplateColums = 'repeat(grid) 1fr';
-
+    container.appendChild(gridCells);
+  }
 }
 
-function resetContainer() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild)
-    }
-}
-
-window.onload = function () {
-    createGrid(grid);
-}
+createGrid(100);
