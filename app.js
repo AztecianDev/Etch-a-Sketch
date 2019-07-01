1d11868a4aebbc8fb1ec9cf2584f64f8f6dd40cf
Az <aztecian1122@gmail.com>
2019-07-01 10:32:55 +0100

    container now displays right amount of cells

--- app.js
+++ app.js
@@ -2,31 +2,24 @@
 const btnReset = document.getElementById("reset");
 const btnBlack = document.getElementById("black");
 const btnRainbow = document.getElementById("rainbow");
-const container = document.querySelector("#container");
-
-//container styles.
-document.getElementById("container").style.display = "flex";
-document.getElementById("container").style.flexFlow = "row wrap";
-document.getElementById("container").style.justifyContent = "flex-start";
-document.getElementById("container").style.border = "1px solid #000";
-document.getElementById("container").style.height = "600px";
-document.getElementById("container").style.width = "600px";
-document.getElementById("container").style.margin = "auto";
+const gridContainer = document.querySelector("#container");
 
 //create grid
 function createGrid(gSize) {
-  for (let i = 0; i < gSize; i++) {
-    let gridCells = document.createElement("div");
-    gridCells.className = "grid-cell";
-    gridCells.style.backgroundColor = "white";
-    gridCells.style.width = "50px";
-    gridCells.style.height = "50px";
-    gridCells.style.margin = "0";
-    gridCells.style.padding = "0";
-    gridCells.style.border = " 1px solid #000";
+  gridContainer.style.gridTemplateColumns = `repeat(${gSize}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gSize}, 1fr)`;
 
-    container.appendChild(gridCells);
+  for (let i = 0; i < gSize * gSize; i++) {
+    gridCells = document.createElement("div");
+    gridCells.classList.add("grid-cell");
+    gridCells.style.backgroundColor = "white";
+
+    gridContainer.appendChild(gridCells);
   }
 }
 
-createGrid(100);
+createGrid(16);
+
+btnBlack.addEventListener("click", function() {
+  alert("hello im working");
+});
